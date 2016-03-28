bfb57b068425f801e9bdeb46684dc053eacd78e7
Snarky <thesnarky1@users.noreply.github.com>
2016-03-28 18:33:56 +0200

    Hacked together quick change from Ironwood to Rome

---
+++ js/constants.js
@@ -0,0 +1,29 @@
+//Constants for app
+var ROME_WIDTH = 100;
+var ROME_HEIGHT = 40;
+
+//Constants for debugging
+var DEBUG_SILENT_MODE = false;
+var DEBUG_SHOW_ALL    = false; //Warning, this gets slow
+var DEBUG_INVIS_MODE  = false;
+
+//Constants for direction
+var DIR_N  = 0;
+var DIR_NE = 1;
+var DIR_E  = 2;
+var DIR_SE = 3;
+var DIR_S  = 4;
+var DIR_SW = 5;
+var DIR_W  = 6;
+var DIR_NW = 7;
+
+//Constants for output
+var MAP_Y_OFFSET = 1;
+var MAP_X_OFFSET = 0;
+
+var STATUSBAR_TEXT_COLOR = "black";
+var STATUSBAR_BACKGROUND_COLOR = "orange";
+
+//Constants for the end game box
+var END_GAME_BOX_WIDTH  = 40;
+var END_GAME_BOX_HEIGHT = 20;
---
+++ js/rome.js
@@ -0,0 +1,41 @@
+var Rome = {
+  game: null,
+
+  init: function() { //Init the required variables
+    window.addEventListener("keypress", this);
+  },
+
+  handleEvent: function(e) {
+    if(e.type == "keypress" && e.keyCode == ROT.VK_RETURN) { //Load the game
+      window.removeEventListener("keypress", this);
+      var displayOptions = {
+        width: ROME_WIDTH,
+        height: ROME_HEIGHT,
+        spacing: .9,
+        fontFamily: "arial"
+      }
+      this.display = new ROT.Display(displayOptions);
+      document.getElementById('content').innerHTML = "";
+      document.getElementById('content').appendChild(this.display.getContainer());
+
+      this.scheduler = new ROT.Scheduler.Simple();
+      this.engine = new ROT.Engine(this.scheduler);
+
+      //Make the game
+      //this.game = new Game(this.WIDTH, this.HEIGHT);
+
+      //this.scheduler.add(this.game.getPlayer(), true);
+      this.engine.start();
+    }
+  },
+
+  getScheduler: function() {
+    return this.scheduler;
+  },
+
+  getEngine: function() {
+    return this.engine;
+  }
+
+
+}
